4f572fb0ca0ae6f4971547ea5311e05fee0821c7
Amelia <millie-service@hotmail.com>
2025-04-14 16:44:58 +0200

    update settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'checkout',
     'crispy_forms',
     'profiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -177,6 +178,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+
+#Bucket config
+if 'USE_AWS' is os.environ:
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado12'
+    AWS_S3_REGION_NAME = 'Europe (Stockholm) eu-north-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    
+# Static storage
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'    
+STATICFILES_LOCATION = 'static'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+MEDIAFILES_LOCATION = 'media'
+
+#Over-ride static and media urls
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Stripe
 FREE_DELIVERY_THRESHOLD= 50
